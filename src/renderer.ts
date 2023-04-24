856271baac7442a2959b39ed29b7d465946f6612
Theo Lounissi <Theo.lounissi@gmail.com>
2023-04-24 20:10:25 +0200

    Add timer and remove DOM elements when not needed anymore

--- src/renderer.ts
+++ src/renderer.ts
@@ -7,7 +7,9 @@
 let showImageFolder = document.getElementById("showImageFolder");
 let startButtonDiv = document.getElementById("startButtonDiv");
 let timeSelectionDiv = document.getElementById("timeSelectionDiv");
-let timeSeletionElement = document.getElementsByName("timeSelected") as NodeListOf<HTMLInputElement>;
+let timeSeletionElement = document.getElementsByName(
+  "timeSelected"
+) as NodeListOf<HTMLInputElement>;
 
 document.getElementById("dirs").addEventListener("click", () => {
   window.postMessage({
@@ -16,26 +18,25 @@ document.getElementById("dirs").addEventListener("click", () => {
 });
 
 window.api.receive("imagesToDisplay", (data: any) => {
-
   let startButton = document.createElement("button");
   let imgList = document.createElement("ul");
   //let selectTimer = document.createElement("select");
-  let timePossible = [30000, 60000, 120000, 300000]
-  let timerSelected : string;
-  let isImgSeleted = new Array(); 
+  let timePossible = [30000, 60000, 120000, 300000];
+  let timerSelected: string;
+  let isImgSeleted = new Array();
 
-  for (const [key, time] of timePossible.entries()) { 
+  for (const [key, time] of timePossible.entries()) {
     let timeOption = document.createElement("input");
     let timeLabel = document.createElement("label");
     timeOption.setAttribute("type", "radio");
     timeOption.setAttribute("id", "time:" + time);
     timeOption.setAttribute("name", "timeSelected");
     timeOption.setAttribute("value", time.toString());
-    if (key === 0){
-        timeOption.checked = true;
-    } 
+    if (key === 0) {
+      timeOption.checked = true;
+    }
     timeLabel.setAttribute("for", "time:" + time);
-    timeLabel.textContent = (time/1000).toString();
+    timeLabel.textContent = (time / 1000).toString();
     timeSelectionDiv.appendChild(timeOption);
     timeSelectionDiv.appendChild(timeLabel);
   }
@@ -44,23 +45,26 @@ window.api.receive("imagesToDisplay", (data: any) => {
   startButton.textContent = "Start";
   startButton.setAttribute("id", "startButton");
   startButton.addEventListener("click", () => {
-
     let imgToSend = new Array();
     for (const time of timeSeletionElement) {
-        if (time.checked) {
-            timerSelected = time.value;
-            console.log(timerSelected);
-        }
+      if (time.checked) {
+        timerSelected = time.value;
+        console.log(timerSelected);
+      }
     }
     for (const check of isImgSeleted) {
-        if (check.checked) { 
-            imgToSend.push(document.getElementById("img" + check.id.slice(-1)).getAttribute("src"));
-        }
-    }    
-    if(imgToSend.length > 0){
-        startImageScrolling(imgToSend, parseInt(timerSelected));
-    }  else{
-        alert("You need to select at least one image.")
+      if (check.checked) {
+        imgToSend.push(
+          document
+            .getElementById("img" + check.id.slice(-1))
+            .getAttribute("src")
+        );
+      }
+    }
+    if (imgToSend.length > 0) {
+      startImageScrolling(imgToSend, parseInt(timerSelected));
+    } else {
+      alert("You need to select at least one image.");
     }
   });
 
@@ -88,7 +92,7 @@ window.api.receive("imagesToDisplay", (data: any) => {
     imgList.appendChild(listElement);
     x++;
   }
-  
+
   for (const child of showImageFolder.children) {
     showImageFolder.removeChild(child);
   }
@@ -96,24 +100,38 @@ window.api.receive("imagesToDisplay", (data: any) => {
 
   if (imgList.childElementCount && !startButtonDiv.childElementCount) {
     startButtonDiv.appendChild(startButton);
-  } else if (!imgList.childElementCount && startButtonDiv.childElementCount ) {
+  } else if (!imgList.childElementCount && startButtonDiv.childElementCount) {
     startButtonDiv.removeChild(startButtonDiv.lastChild);
   }
 });
 
 function startImageScrolling(imgList: string[], durationTimer: number) {
-    showImageFolder.innerHTML = "";
-    startButtonDiv.innerHTML = "";
-    let imagePlace = document.createElement('img');
-    let x = 0;
-    showImageFolder.appendChild(imagePlace);
+  document.getElementById("selectFolderDiv").style.display = "none";
+  showImageFolder.innerHTML = "";
+  startButtonDiv.innerHTML = "";
+  let imagePlace = document.createElement("img");
+  let x = 0;
+  let timer = durationTimer/1000;
+  let timerDiv = document.createElement("div");
+  showImageFolder.appendChild(timerDiv);
+  showImageFolder.appendChild(imagePlace);
+  imagePlace.setAttribute("src", imgList[x]);
+  let imgTimer = setInterval(() => {
+
+    timerDiv.innerHTML = (timer - 1).toString();
+    timer--;
+    console.log(imgList.length - 1);
     imagePlace.setAttribute("src", imgList[x]);
-    let imgTimer = setInterval(() => {
-        x++;
-        imagePlace.setAttribute("src", imgList[x]);
-        if(x >= imgList.length - 1){
-            setTimeout(() => {showImageFolder.innerHTML = "";}, durationTimer)
-            clearInterval(imgTimer);
-        }
-    }, durationTimer)
+
+    if (timer < 0) {
+      x++;
+      timer = durationTimer/1000;
+      if (x > imgList.length - 1) {
+        imagePlace.remove();
+        timerDiv.remove();
+        document.getElementById("selectFolderDiv").style.display = "block";
+        clearInterval(imgTimer);
+      }
+    }
+  }, 1000);
 }
